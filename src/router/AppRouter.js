bb1199423955ec89e065a1501aadec3bacfb84c4
Luis Chilo <gfxargentina@gmail.com>
2021-04-13 11:37:48 -0300

    add routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+  
 
 export const AppRouter = () => {
     return (
-        <div>
-            <h1>App Router</h1>
-        </div>
+        <Router>
+            <div>            
+                <Switch>
+                    <Route exact path="/login" component={ LoginScreen } />
+                    <Route exact path="/" component={ CalendarScreen } />
+
+                    {/* redirecciona al home si no se encuentra la url */}
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+      </Router>
     )
 }
